2194264c79e173b0694c1e53f8dc71855b5bb733
Jonathan Guthrie <jguthrie@brokersys.com>
2018-09-09 13:32:57 -0500

    Add a UI-only mode so that I can work on the user interface more easily and start to implement the user interface.
    Starting with the columns of buttons for the recorded messages

--- keyer.py
+++ keyer.py
@@ -12,6 +12,32 @@ import serial
 # import io
 from morse_to_text import MorseDecoder, MorseEncoder
 
+class MemoryButton(tkinter.ttk.Button):
+    def __init__(self, master, **kwargs):
+        super(MemoryButton, self).__init__(master, **kwargs)
+        # print(self.text)
+
+class MemoryButtons(tkinter.ttk.Frame):
+    def __init__(self, master, startCount, numButtons, **kwargs):
+        super(MemoryButtons, self).__init__(master, **kwargs)
+        self.columnconfigure(0, weight=1)
+        self.buttons = []
+        for i in range(numButtons):
+            b = MemoryButton(self, text="F%d"%(i+startCount))
+            self.rowconfigure(i, weight=1)
+            b.grid(column=0, row=i, padx=2, pady=2, sticky=(tkinter.N, tkinter.S))
+            self.buttons.append(b)
+
+class TopBar(tkinter.ttk.Frame):
+    def __init__(self, master, **kwargs):
+        super(TopBar, self).__init__(master, **kwargs)
+
+class TransmitterButtons(tkinter.ttk.Frame):
+    def __init__(self, master, **kwargs):
+        super(TransmitterButtons, self).__init__(master, **kwargs)
+
+
+# NOTE:  The display I want to configure for is 1024x600 pixels
 class mainWindow(tkinter.ttk.Frame):
     def __init__(self, top, encoder, **kwargs):
         self.encoder = encoder
@@ -20,8 +46,16 @@ class mainWindow(tkinter.ttk.Frame):
         top.rowconfigure(0, weight=1)
         top.columnconfigure(0, weight=1)
         self.grid(column=0, row=0, sticky=(tkinter.N, tkinter.W, tkinter.E, tkinter.S))
+        self.left = MemoryButtons(self, 1, 6)
+        self.right = MemoryButtons(self, 7, 6)
+        self.left.grid(row=0, column=0, rowspan=3, sticky=(tkinter.N, tkinter.S))
+        self.right.grid(row=0, column=2, rowspan=3, sticky=(tkinter.N, tkinter.S))
+        self.topBar = TopBar(self)
+        self.topBar.grid(row=0, column=1)
+        self.xmitters = TransmitterButtons(self)
+        self.xmitters.grid(row=1, column=1)
         self.ta = tkinter.scrolledtext.ScrolledText(self)
-        self.ta.grid(row=0, column=0)
+        self.ta.grid(row=2, column=1)
         self.bind_all('<Alt-Key-q>', self.getout)
         self.bind_all('<Key>', self.getout)
 
@@ -37,7 +71,6 @@ class mainWindow(tkinter.ttk.Frame):
             quit()
         if (16 == 0xfe & event.state):
             self.encoder.one_letter(1, event.char)
-
 
 # Note:  If I'm going to allow the system to drive multiple transmitters
 # simultaneously, I'm going to have to work out a mechanism for dealing
@@ -118,8 +151,17 @@ class recvThread(threading.Thread):
                     # stdout.write(c)
                     # stdout.flush()
 
-port = serial.Serial('/dev/ttyUSB0', 115200, timeout=30, xonxoff=True)
-# sio = io.TextIOWrapper(io.BufferedRWPair(port, port))
+import argparse
+
+parser = argparse.ArgumentParser(description="Telegraph Keyer User Interface")
+parser.add_argument("--ui", help="Run the UI without trying to talk to a keyer", action='store_true')
+parser.add_argument("--serial", nargs='?', help="The serial port to use to talk to the dongle", default="/dev/ttyUSB0")
+
+args = parser.parse_args()
+
+if not args.ui:
+    port = serial.Serial(args.serial, 115200, timeout=30, xonxoff=True)
+    # sio = io.TextIOWrapper(io.BufferedRWPair(port, port))
 
 xmit_queue = queue.Queue()
 decoder = MorseDecoder()
@@ -129,16 +171,18 @@ top = tkinter.Tk()
 top.title = "Memory Keyer"
 frame = mainWindow(top, encoder, padding="3 3 12 12")
 
-thread1 = xmitThread(1, port, xmit_queue)
-thread1.start()
-thread2 = recvThread(1, port, decoder, thread1, frame)
-thread2.start()
+if not args.ui:
+    thread1 = xmitThread(1, port, xmit_queue)
+    thread1.start()
+    thread2 = recvThread(1, port, decoder, thread1, frame)
+    thread2.start()
 
 top.mainloop()
 
-port.close()
+if not args.ui:
+    port.close()
 
-thread2.endThread()
-thread2.join()
-thread1.endThread()
-thread1.join()
+    thread2.endThread()
+    thread2.join()
+    thread1.endThread()
+    thread1.join()
